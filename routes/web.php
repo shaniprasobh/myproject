46b82511d841ca502be236e990592745e4a50c49
github.com/shaniprasobh <shaniprasobh@gmail.com>
2025-09-22 14:08:38 +0530

    Initial Laravel 12 setup with AdminLTE and permissions

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false]); // disables registration
+
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
