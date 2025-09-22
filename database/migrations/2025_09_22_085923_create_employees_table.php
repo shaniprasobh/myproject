938e0c9e9a01a76480f0501c8e640579b64790d8
github.com/shaniprasobh <shaniprasobh@gmail.com>
2025-09-22 14:35:11 +0530

    Create employees table and Employee model

---
+++ database/migrations/2025_09_22_085923_create_employees_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+    $table->id();
+    $table->integer('user_id')->default(0);
+    $table->foreignId('company_id')->nullable()->constrained('companies')->nullOnDelete();
+    $table->string('name')->nullable();
+    $table->string('email')->nullable();
+    $table->string('mobile_number', 15)->nullable();
+    $table->string('landline_number', 15)->nullable();
+    $table->mediumText('address')->nullable();
+    $table->integer('country_id')->default(1);
+    $table->integer('state_id')->default(1);
+    $table->string('location')->nullable();
+    $table->string('profile_picture')->nullable();
+    $table->string('employee_code')->nullable();
+    $table->integer('designation_id')->nullable();
+    $table->integer('department_id')->nullable();
+    $table->date('doj')->nullable();
+    $table->tinyInteger('status')->default(1);
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
